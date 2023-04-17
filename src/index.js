eef3af8108fe9e26e538ba2ba05095fc13334ef7
kellmcallister <128011075+kellmcallister@users.noreply.github.com>
2023-04-17 16:33:13 -0500

    Edited links and added JS

---
+++ src/index.js
@@ -0,0 +1,8 @@
+function voteAlert() {
+  alert("That's my favorite too!");
+}
+
+document.querySelector("#vote-image1").addEventListener("click", voteAlert);
+document.querySelector("#vote-image2").addEventListener("click", voteAlert);
+document.querySelector("#vote-image3").addEventListener("click", voteAlert);
+document.querySelector("#vote-image4").addEventListener("click", voteAlert);
